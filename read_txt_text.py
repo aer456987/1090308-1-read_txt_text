0f2e68b7a8fad94e18d0bcd152612defae75f388
Hua Yen <aer456987@gmail.com>
2020-04-14 01:14:03 +0800

    frsit text

---
+++ read_txt_text.py
@@ -0,0 +1,10 @@
+data = []
+count = 0
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line.strip())
+		count += 1
+		if count % 1000 == 0:
+			print(len(data))
+
+print('計算完成,共', len(data), '筆資料') 
